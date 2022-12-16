6f6d66145860f878893d166f0cbedddf70f9a219
Abdessamed <ka_seddiki@esi.dz>
2022-12-16 16:28:05 +0100

    Mycommit

---
+++ src/Components/Navbar.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { CgAdd } from "react-icons/cg";
+
+
+function Navbar(){
+    return(
+        <>
+        <Navbar class="navbar">
+            <div class="navbar-logo">
+                <img></img>
+            </div>
+
+            <div class="list">
+                <div class="items-list">
+                    <a href="">Accueil</a>
+                    <a href="">Aide</a>
+                    <a href="">Compte</a>
+                </div>
+                <div class="button-dep">
+                    <button>
+                        <CgAdd /> <span>Déposer Annonce</span>
+                    </button>
+                </div>
+            </div>
+        </Navbar>
+        </>
+    )
+}
+
+export default Navbar;
